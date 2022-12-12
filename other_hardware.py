5074afb321e92b4ecd0a63d7f80be482be06ee2e
Pedro Sodré <sodre2306@gmail.com>
2022-12-12 00:14:58 -0300

    feat: user info

---
+++ other_hardware.py
@@ -0,0 +1,76 @@
+#Teste 1 de GPU
+# import igpu
+
+# gpu_count = igpu.count_devices()
+# gpu = igpu.get_device(0)
+# print(f'This host has {gpu_count} devices.')
+# print(f'The first gpu is a {gpu.name} with {gpu.memory.total:.0f}{gpu.memory.unit}.')
+
+
+
+#Teste 2 de GPU
+# _GPU = False
+# _NUMBER_OF_GPU = 0
+
+# def _check_gpu():
+#     global _GPU
+#     global _NUMBER_OF_GPU
+#     nvidia_smi.nvmlInit()
+#     _NUMBER_OF_GPU = nvidia_smi.nvmlDeviceGetCount()
+#     if _NUMBER_OF_GPU > 0:
+#         _GPU = True
+
+# def _print_gpu_usage(detailed=False):
+
+#     if not detailed:
+#         for i in range(_NUMBER_OF_GPU):
+#             handle = nvidia_smi.nvmlDeviceGetHandleByIndex(i)
+#             info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
+#             print(f'GPU-{i}: GPU-Memory: {_bytes_to_megabytes(info.used)}/{_bytes_to_megabytes(info.total)} MB')
+
+# def _bytes_to_megabytes(bytes):
+#     return round((bytes/1024)/1024,2)
+
+# if __name__ == '__main__':
+#     print('Checking for Nvidia GPU\n')
+#     _check_gpu()
+#     if _GPU:
+#         _print_gpu_usage()
+#     else:
+#         print("No GPU found.")
+
+import psutil
+
+battery = psutil.sensors_battery()
+users = psutil.users()
+temperatures = psutil.sensors_temperatures()
+#print(temperatures.nvme)
+
+def get_user_name():
+    return users[0].name
+
+def get_user_pid():
+    return users[0].pid #pid do processo de login
+
+
+def secs2hours(secs):
+    if battery == None:
+        return ""
+    mm, ss = divmod(secs, 60)
+    hh, mm = divmod(mm, 60)
+    return "%d:%02d:%02d" % (hh, mm, ss)
+
+def get_battery_percent():
+    if battery == None:
+        return ""
+    return battery.percent
+
+def get_battery_time_left():
+    if battery == None:
+        return ""
+    return secs2hours(battery.secsleft)
+
+def get_battery_plugged():
+    if battery == None:
+        return ""
+    return battery.power_plugged
--- static_info.py
+++ static_info.py
@@ -8,15 +8,19 @@ from dash import Output, Input, html
 
 import multiOS
 
+import other_hardware
 
 def get_component(app):
 
     @app.callback(Output('battery', 'children'), [Input('graph-update', 'n_intervals')])
     def update_table(n_intervals):
         dados = psutil.sensors_battery()
+        if dados == None:
+            return ""
         return f"percentagem de bateria: {round(dados.percent, 2)}% {'(carregando)' if dados.power_plugged else ''}"
 
     return html.Div([
-        html.H3(f"SO: {platform.system()}   processador: {multiOS.get_processor_name()} Tipo da máquina: {platform.machine()}   Nome de rede: {platform.node()}"),
+        html.H3(f"SO: {platform.system()}    //  processador: {multiOS.get_processor_name()} //  Tipo da máquina: {platform.machine()}    //  Nome de rede: {platform.node()}"),
+        html.H3(f'Usuário: {other_hardware.get_user_name()}  // PID Login: {other_hardware.get_user_pid()}'),
         html.H3(id="battery")
     ])
